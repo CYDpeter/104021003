0d9eec8ddc6f54855eb82a8e6897b29163e393be
CYDpeter <qw981901@yahoo.com.tw>
2015-08-04 11:43:14 +0800

    A2alter
    homework2

--- Homework2/src/A2.java
+++ Homework2/src/A2.java
@@ -4,12 +4,12 @@ public class A2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		System.out.print("請輸入底");
+		System.out.print("請輸入三角形底");
 		Float b = scn.nextFloat();
-		System.out.print("請輸入高");
+		System.out.print("請輸入三角形高");
 		Float h = scn.nextFloat();
 		Float A = b*h/2;
-		System.out.print("面積為"+A);
+		System.out.print("三角形面積為"+A);
 	}
 
 }
