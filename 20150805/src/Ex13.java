b31ae192e600cec3038831951a65ab7a417d5751
CYDpeter <qw981901@yahoo.com.tw>
2015-08-05 12:10:59 +0800

    Ex13
    2 1

---
+++ 20150805/src/Ex13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ex13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("叫块タ俱计");
+		int a = scn.nextInt();
+		if(a%2==0){
+			System.out.print(a+"案计");
+		}else{
+			System.out.print(a+"计");
+		}
+	}
+
+}
