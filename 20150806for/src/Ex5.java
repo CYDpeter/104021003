77d4fe4345f06b01353e6b8e14721c3141bef6f8
CYDpeter <qw981901@yahoo.com.tw>
2015-08-06 15:32:49 +0800

    ForEx5
    M N

---
+++ 20150806for/src/Ex5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入第一個數");
+		int a = scn.nextInt();
+		System.out.print("請輸入第二個數");
+		int b = scn.nextInt();
+
+	}
+
+}
