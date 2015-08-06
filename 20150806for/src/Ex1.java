fa48c713fe7f746c9bba850d712e4c5947d62a1f
CYDpeter <qw981901@yahoo.com.tw>
2015-08-06 14:44:53 +0800

    ForEx1
    13 1399

---
+++ 20150806for/src/Ex1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("½Ð¿é¤J13");
+		int a = scn.nextInt();
+		int sum = 0;
+		for(a=13;a<=100&&a>=13;a=a+1){
+			sum = sum+a;			
+		}
+		System.out.print("Sum="+sum);
+	}
+
+}
