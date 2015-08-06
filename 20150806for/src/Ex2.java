f75f46369d4368a04f6bc5f61ecb275e65cac26c
CYDpeter <qw981901@yahoo.com.tw>
2015-08-06 15:00:02 +0800

    ForEx2
    13 1399

---
+++ 20150806for/src/Ex2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("½Ð¿é¤J13");
+		int a =scn.nextInt();
+		int i ;
+		int sum = 0;
+
+			for(i=13;i<=1399;i=i+2){
+				sum=sum+i;
+			}
+			System.out.print("Sum="+sum);
+
+		}
+	
+				
+					
+		
+	
+}
