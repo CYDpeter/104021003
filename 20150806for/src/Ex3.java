386da57313716e4636fae81ab420e91e91832d0b
CYDpeter <qw981901@yahoo.com.tw>
2015-08-06 16:02:44 +0800

    Ex3change
    17

--- 20150806for/src/Ex3.java
+++ 20150806for/src/Ex3.java
@@ -4,11 +4,11 @@ public class Ex3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		System.out.print("½Ð¿é¤J17");
-		int i = scn.nextInt();
 		int sum =0 ;
-		for(i=17;i<=1399;i=i+17){
-			sum = sum+i;
+		for(int i=13;i<=1399;i=i+1){
+			if(i%17 == 0){
+				sum=sum+i;
+			}
 		}
 		System.out.print("Sum="+sum);
 	}
