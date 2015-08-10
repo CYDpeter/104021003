798369f455755472dd7cb738fe0e3217b7b9625c
CYDpeter <qw981901@yahoo.com.tw>
2015-08-10 15:26:25 +0800

    Whileex1chang2

--- 20150810while/src/Whileex1.java
+++ 20150810while/src/Whileex1.java
@@ -8,10 +8,10 @@ public class Whileex1 {
 		float k = 20000;
 		while(k<1100000){//500-(500*75%)=1100000
 			
-			k = (float)((int)k+k*1.002);
+			k = (float)((int)k+k*0.15);
 			m++;
 		}
-		System.out.print("下午"+(m+3)+"點，這個時間到達大於等於水位 ");
+		System.out.print("約"+m+"個小時，這個時間到達大於等於水位 ");
 	}
 
 }
