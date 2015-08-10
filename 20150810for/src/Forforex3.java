131592f467cbaba2c401932e821d240c40f493b9
CYDpeter <qw981901@yahoo.com.tw>
2015-08-10 10:56:33 +0800

    Forforex3
    #*

---
+++ 20150810for/src/Forforex3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Forforex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int k = 1;
+		for(int i=0;i<n;i++){
+			System.out.println();
+			for(int j=0;j<n-i-1;j++){
+				System.out.print("#");
+			}
+			for(int m=0;m<=2*i;m++){
+				for(int l=0;l<=2*i-1;l++){
+		}
+			System.out.print("*");
+	}
+	}
+}
+}
