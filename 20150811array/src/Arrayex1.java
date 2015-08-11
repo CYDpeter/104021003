747d05d738c248ede69092eef745a9175e5766bd
CYDpeter <qw981901@yahoo.com.tw>
2015-08-11 15:11:48 +0800

    Arrayex1
    10 10%2

--- 20150811array/src/Arrayex1.java
+++ 20150811array/src/Arrayex1.java
@@ -7,35 +7,35 @@ public class Arrayex1 {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		Random rnd = new Random();
-		int count = 0;
-		int[] data = new int[10];
-		for (int i = 0; i < 10; i++) {
-			data[i] = (rnd.nextInt(100) + 1);
-			if (data[i] % 2 == 0) {
-				count++;
+		int count = 0;//從0開始
+		int[] data = new int[10];//要10個變數
+		for (int i = 0; i < 10; i++) {//從0開始到9共10並累加
+			data[i] = (rnd.nextInt(100) + 1);//1-100從中隨機找
+			if (data[i] % 2 == 0) {//假如隨機數被2整除
+				count++;//找完一個後繼續下一個
 			}
-		}
-		int j = 0;
-		while (j < 3) {
-			if (j == 0) {
-				System.out.println("有幾個偶數");
+		}//執行到隨機找完10個後往下做
+		int j = 0;//設j為整數並從0開始
+		while (j < 3) {//while迴圈j做3次(0~2)
+			if (j == 0) {//假如j為0時
+				System.out.println("有幾個偶數");//顯示題目
 			}
-			int k = scn.nextInt();
-			if (k == count) {
-				j = 3;
-				System.out.print("恭喜猜對");
-			} else {
-				if (j < 2) {
-					System.out.println("請繼續輸入");
-				} else {
-					System.out.println("失敗");
+			int k = scn.nextInt();//設k為整數為使用者輸入
+			if (k == count) {//假如k=亂數中有幾個偶數
+				j = 3;//此時j=3
+				System.out.print("恭喜猜對");//顯示猜對並結束迴圈
+			} else {//若沒猜中
+				if (j < 2) {//假如j=為0或1(第二次猜為0第三次猜為1)
+					System.out.println("請繼續輸入");//顯示請繼續輸入
+				} else {//若3次都沒中
+					System.out.println("失敗");//顯示失敗
 				}
-				j++;
+				j++;//猜錯就一直做到j<3(3次)
 			}
-		}
-		System.out.println("偶數有" + count + "個");
-		for (int i = 0; i < 10; i++) {
-			System.out.println(data[i]);
+		}//猜錯三次結束迴圈
+		System.out.println("偶數有" + count + "個");//成功和失敗都顯示共有幾個隨機偶數
+		for (int i = 0; i < 10; i++) {//從0開始到9共10並累加
+			System.out.println(data[i]);//顯示所有隨機亂數
 		}
 	}
 }
