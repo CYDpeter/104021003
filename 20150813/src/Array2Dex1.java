795bdbbdaa8d3332852df3ff04afe785e979f543
CYDpeter <qw918901@yahoo.cmm.tw>
2015-08-13 13:35:39 +0800

    Array2Dex1
    Max min

---
+++ 20150813/src/Array2Dex1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Array2Dex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("班上有幾人");
+		int n = scn.nextInt();// 輸入使用者希望輸入人數和科目
+		float min = 100, Max = 0;
+		float data[][] = new float[n][4];// 陣列隨使用者輸入範圍去執行
+		String name[] = new String[n];// 人數隨使用者輸入範圍去執行
+		for (int i = 0; i < n; i++) {
+			System.out.print("請輸入第" + (i + 1) + "位同學名字");
+			name[i] = scn.next();
+			System.out.print("請輸入三科成績");
+			data[i][0] = scn.nextFloat();
+			data[i][1] = scn.nextFloat();
+			data[i][2] = scn.nextFloat();
+			data[i][3] = (data[i][0] + data[i][1] + data[i][2]) / 3;
+		} // 輸入完成所有人名字和成績和個人平均後
+		System.out.println("全班資料如下:");
+		for (int i = 0; i < n; i++) {
+			System.out.println(name[i] + "\t" + data[i][0] + "\t" + data[i][1] + "\t" + data[i][2] + "\t" + data[i][3]);
+		}
+		for (int i = 0; i < n; i++) {
+			if (data[i][3] < 60) {
+				System.out.println(name[i] + "不及格");
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (data[i][3] < min) {
+				min = data[i][3];
+				System.out.println("最低分為" + min);
+			} else {
+				if (data[i][3] > Max) {
+					Max = data[i][3];
+					System.out.println("最高分為" + Max);
+				}
+			}
+		}
+	}
+}
