fc27d54e2c8cedb86d7c2804a1e8457eeb88f6e9
CYDpeter <qw981901@yahoo.com.tw>
2015-08-13 22:51:39 +0800

    20150813home
    stars

---
+++ 20150813home/src/Method.java
@@ -0,0 +1,15 @@
+
+public class Method {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		star();// ㊣star()ㄧ计
+		System.out.println("Knowledge is powwer");
+		star();// ㊣star()ㄧ计
+	}// {}ずmainㄧ计
+
+	public static void star() {// starㄧ计
+		for (int i = 0; i < 15; i++)// 癹伴眖i=0秨﹍暗暗i=14氨ゎi患糤
+			System.out.print("「");// 琍腹
+			System.out.print("\n");// 传︽
+	}// {}starㄧ计
+}
