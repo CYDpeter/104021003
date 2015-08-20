fd1ed09a60c36b5f0ad0c1278e6281b42d33c52c
CYDpeter <qw981901@yahoo.com.tw>
2015-08-20 14:30:15 +0800

    text

---
+++ 20150820swing/src/Swingtext1.java
@@ -0,0 +1,10 @@
+
+public class Swingtext1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Swingtext2 mFrm = new Swingtext2();
+		mFrm.setVisible(true);
+	}
+
+}
