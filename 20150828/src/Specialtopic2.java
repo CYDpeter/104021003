1a604e5aeaf8fbff224e74642068186b32a7727e
CYDpeter <qw981901@yahoo.com.tw>
2015-08-24 01:43:53 +0800

    Specialtopic

---
+++ 20150828/src/Specialtopic2.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Specialtopic2 {
+	private JButton jbtnStop1 = new JButton("Stop1");// Stop1的按鈕
+	private JButton jbtnStop2 = new JButton("Stop2");// Stop2的按鈕
+	private JButton jbtnStop3 = new JButton("Stop3");// Stop3的按鈕
+	private JLabel jlb1 = new JLabel();// 標籤物件
+	private Container cp;// 宣告一個視窗盤子(容器)
+	int time = 100;// 轉的速度
+	static ImageIcon pic[] = new ImageIcon[3];//建立ImageIcon陣列
+	
+
+}
